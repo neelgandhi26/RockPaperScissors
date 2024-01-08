629ff26d1fe3a595412ec47e3138d2fd4217dc44
neelgandhi26 <66537511+neelgandhi26@users.noreply.github.com>
2024-01-07 20:06:53 -0500

    Refactored playRound function

--- main.js
+++ main.js
@@ -7,26 +7,16 @@ playRound = (playerSelection, computerSelection) => {
     playerSelection = playerSelection.toUpperCase()
     computerSelection = computerSelection.toUpperCase()
 
-    if (playerSelection == computerSelection) {
+    if (playerSelection === computerSelection) {
         return `Its a tie! You both selected ${playerSelection}`
     }
-    if (playerSelection == 'ROCK' && computerSelection=='PAPER') {
-        return `You Lose! Paper beats Rock`
+    if ((playerSelection === 'ROCK' && computerSelection === 'SCISSORS') ||
+        (playerSelection === 'SCISSORS' && computerSelection === 'PAPER') ||
+        (playerSelection === 'PAPER' && computerSelection === 'ROCK')) {
+        return `You Win! ${playerSelection} beats ${computerSelection}`
     }
-    if (playerSelection == 'ROCK' && computerSelection=='SCISSORS') {
-        return `You Win! Rock beats Scissors`
-    }
-    if (playerSelection == 'PAPER' && computerSelection=='ROCK') {
-        return `You Win! Paper beats rock`
-    }
-    if (playerSelection == 'PAPER' && computerSelection=='SCISSORS') {
-        return `You Lose! Scissors beats Paper`
-    }
-    if (playerSelection == 'SCISSORS' && computerSelection=='PAPER') {
-        return `You Win! Scissors beats Paper`
-    }
-    if (playerSelection == 'SCISSORS' && computerSelection=='ROCK') {
-        return `You Lose! Rock beats Scissors`
+    else {
+        return `You Lose! ${computerSelection} beats ${playerSelection}`
     }
 }
 
