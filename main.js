b87a128699c29e6698bf2dbbc4ec42bbfc37b9c2
neelgandhi26 <66537511+neelgandhi26@users.noreply.github.com>
2024-01-14 20:05:07 -0500

    First design version of app

--- main.js
+++ main.js
@@ -1,25 +1,52 @@
 getComputerChoice = () => {
-    let choices = ['Rock', 'Paper', 'Scissors']
-    return choices[Math.floor(Math.random() * 3)]
-}
+  let choices = ["Rock", "Paper", "Scissors"];
+  return choices[Math.floor(Math.random() * 3)];
+};
 
 playRound = (playerSelection, computerSelection) => {
-    playerSelection = playerSelection.toUpperCase()
-    computerSelection = computerSelection.toUpperCase()
+  playerSelection = playerSelection.toUpperCase();
+  computerSelection = computerSelection.toUpperCase();
 
-    if (playerSelection === computerSelection) {
-        return `Its a tie! You both selected ${playerSelection}`
-    }
-    if ((playerSelection === 'ROCK' && computerSelection === 'SCISSORS') ||
-        (playerSelection === 'SCISSORS' && computerSelection === 'PAPER') ||
-        (playerSelection === 'PAPER' && computerSelection === 'ROCK')) {
-        return `You Win! ${playerSelection} beats ${computerSelection}`
-    }
-    else {
-        return `You Lose! ${computerSelection} beats ${playerSelection}`
-    }
-}
+  if (playerSelection === computerSelection) {
+    playRound(
+      prompt("Pick either Rock, Paper, or Scissors"),
+      getComputerChoice()
+    );
+  }
+  if (
+    (playerSelection === "ROCK" && computerSelection === "SCISSORS") ||
+    (playerSelection === "SCISSORS" && computerSelection === "PAPER") ||
+    (playerSelection === "PAPER" && computerSelection === "ROCK")
+  ) {
+    return "Player";
+  } else {
+    return "Computer";
+  }
+};
 
-const playerSelection = 'rock';
-const computerSelection = getComputerChoice();
-console.log(playRound(playerSelection,computerSelection))
+game = () => {
+  let playerScore = 0;
+  let ComputerScore = 0;
+  for (let i = 0; i < 5; i++) {
+    let playerSelection = prompt("Pick either Rock, Paper, or Scissors");
+    let computerSelection = getComputerChoice();
+
+    let roundWinner = playRound(playerSelection, computerSelection);
+
+    if (roundWinner == "Player") {
+      console.log(`You Win! ${playerSelection} beats ${computerSelection}`);
+      playerScore++;
+    }
+    if (roundWinner == "Computer") {
+      console.log(`You Lose! ${computerSelection} beats ${playerSelection}`);
+      ComputerScore++;
+    }
+  }
+  if (playerScore > ComputerScore) {
+    console.log("You win the game!");
+  } else {
+    console.log("You lost the game!");
+  }
+};
+
+game();
